1ab32cdd13bd20d83618528d7f1c003f686a5a48
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-04-20 15:27:02 +0200

    fix(k3s): remove leftover agenix secretsDir ref from common.nix

--- infra/nixos/modules/k3s/common.nix
+++ infra/nixos/modules/k3s/common.nix
@@ -1,12 +1,5 @@
-{ config, pkgs, meta, secretsDir ? ../../secrets, ... }:
+{ ... }:
 {
-  age.secrets.k3s-token = {
-    file = "${secretsDir}/k3s-token.age";
-    mode = "0400";
-    owner = "root";
-    group = "root";
-  };
-
   systemd.services.k3s = {
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
