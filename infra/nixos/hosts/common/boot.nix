b1a5ca0bd321032316bae865b2f285383a9e4cbf
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-04-20 15:54:00 +0200

    fix(infra): switch to legacy bios grub with bios boot partition

--- infra/nixos/hosts/common/boot.nix
+++ infra/nixos/hosts/common/boot.nix
@@ -1,11 +1,8 @@
 { ... }:
 {
   boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.grub = {
     enable = true;
-    device = "nodev";
-    efiSupport = true;
-    efiInstallAsRemovable = true;
+    device = "/dev/sda";
   };
 }
