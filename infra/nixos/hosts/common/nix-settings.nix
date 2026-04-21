1073a7b9b05ebe97f37e6cf6118e0279021b633d
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-04-21 15:43:30 +0200

    fix(infra): force nix tarball refresh to always fetch latest branch commit

--- infra/nixos/hosts/common/nix-settings.nix
+++ infra/nixos/hosts/common/nix-settings.nix
@@ -1,11 +1,5 @@
 { ... }:
 {
-  documentation.enable = false;
-
-  nixpkgs.overlays = [(final: prev: {
-    python312 = prev.python312.override { enableDoc = false; };
-  })];
-
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "@wheel" ];
