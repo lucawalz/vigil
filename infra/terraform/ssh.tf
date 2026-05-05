1bab7699ba661b0d013766548decbd6b7ad20c1d
Luca Walz <148595155+lucawalz@users.noreply.github.com>
2026-05-05 20:08:41 +0200

    fix(terraform): scope ssh key name per group to allow parallel runs

--- infra/terraform/ssh.tf
+++ infra/terraform/ssh.tf
@@ -1,4 +1,4 @@
 resource "hcloud_ssh_key" "operator" {
-  name       = "vigil-operator"
+  name       = "vigil-operator-${var.group_name}"
   public_key = trimspace(file(var.ssh_public_key_path))
 }
