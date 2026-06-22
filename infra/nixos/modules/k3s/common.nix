0fafe685a3cdafaa2fb0ae69024efbb35ca5b55e
vigil-remediation <remediation@vigil.local>
2026-06-22 20:21:13 +0200

    fix(remediation): add missing ExecStart to k3s.service in common.nix

--- infra/nixos/modules/k3s/common.nix
+++ infra/nixos/modules/k3s/common.nix
@@ -2,6 +2,9 @@
 {
   systemd.services.k3s = {
     wants = [ "network-online.target" ];
-    after = [ "network-online.target" ];
+    after  = [ "network-online.target" ];
+    serviceConfig = {
+      ExecStart = "/run/current-system/sw/bin/k3s agent";
+    };
   };
 }
